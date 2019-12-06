74a4c7ecc3baf168d5764f7086d2dd99e5af4c98
Omar <ogspain@gmail.com>
2019-12-05 20:43:49 -0400

    Documentation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Service } from './src'
 
 
 const function genServices(list) {
+  // Generates a list od services
   let serv_list = []
   for (let i = 0; i < list.length; i++) {
     serv_list.push(Service(list[i].link, list[i].name ))

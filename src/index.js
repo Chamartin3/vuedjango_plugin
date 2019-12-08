ce6b7e4d23fbf6dd962a6b05ca893f91fe15bfa9
Omar <ogspain@gmail.com>
2019-12-08 10:54:33 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+// import Base from './BaseService'
+import Auth from './AutenticationService'
 import APIModel from './APIService'
 
 Vue.prototype.$django = {...DJANGO_CONTEXT}
@@ -28,6 +30,26 @@ function processParams(params) {
   return param_names
 }
 
+// function generateAPIActions(actionlist) {
+  // let actions={}
+  // let asembleURL=asembleURL
+  // for (var i = 0; i < actionlist.length; i++) {
+  //   let param_names=processParams(actionlist[i].fullpath.params)
+  //   let type = actionlist[i].type
+  //   let service = new Base(actionlist[i].basepath)
+  //   actions[actionlist[i].name]
+
+      // return actionlist[1].type
+    // actions[actionlist[i].name]=new Function([...param_names],
+    //   'let pieces = actionlist[i].fullpath.pieces; asembleURL(pieces); return arguments'
+    // )
+    //   // return actionlist[1].type
+
+
+  // return actions
+
+// }
+
 function generateAPIModels() {
   let mappedActions=DJANGO_CONTEXT._actions
   let token=DJANGO_CONTEXT.csrf_token
@@ -39,29 +61,39 @@ function generateAPIModels() {
   Vue.prototype.$django.models= apiModels
 }
 
-async function getModels() {
-  // Gert all the root api views (only work with viewsets)
-  let base_urls=[]
-  let root_routes=DJANGO_CONTEXT.model_routes
-  for (var i = 0; i < root_routes.length; i++) {
-    let url=DJANGO_CONTEXT.api_route+'/'+root_routes[i]
-    let serv=new Base(url)
-    base_urls.push(serv.list())
-  }
-  let services = await Promise.all(base_urls)
-  let all_serv={}
 
-  for (var i = 0; i < services.length; i++) {
-    if(typeof(services[i])==='object'){
-      all_serv={...all_serv,...services[i]}
-    }
-  }
-
-  let models={}
-  Object.entries(all_serv).forEach(([key,value])=>{
-    models[key]= new Base(value, key, true)
-  })
-  Vue.prototype.$django.MVSmodels=models
+function getAutentication() {
+  let AUTH = new Auth(DJANGO_CONTEXT.autentication.auth_path, DJANGO_CONTEXT.autentication.auth_path, DJANGO_CONTEXT.autentication.on_logout)
+  Vue.prototype.$django.auth= AUTH
 }
 
+// async function getModels() {
+//   // Gert all the root api views (only work with viewsets)
+//   let base_urls=[]
+//   let root_routes=DJANGO_CONTEXT.model_routes
+//   for (var i = 0; i < root_routes.length; i++) {
+//     let url=DJANGO_CONTEXT.api_route+'/'+root_routes[i]
+//     let serv=new Base(url)
+//     base_urls.push(serv.list())
+//   }
+//   let services = await Promise.all(base_urls)
+//   let all_serv={}
+//
+//   for (var i = 0; i < services.length; i++) {
+//     if(typeof(services[i])==='object'){
+//       all_serv={...all_serv,...services[i]}
+//     }
+//   }
+//
+//   let models={}
+//   Object.entries(all_serv).forEach(([key,value])=>{
+//     models[key]= new Base(value, key, true)
+//   })
+//   Vue.prototype.$django.MVSmodels=models
+// }
+
+// getModels()
+getAutentication()
+generateAPIModels()
 window.django=Vue.prototype.$django
+
